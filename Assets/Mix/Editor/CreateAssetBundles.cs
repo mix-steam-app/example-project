0c69605cd4a23ffc58b9c7e1bee9d1e249a04953
mix-steam-app <mix.steam.app@gmail.com>
2024-12-21 12:43:41 -0600

    Add initial asset bundle creation script and associated metadata

---
+++ Assets/Mix/Editor/CreateAssetBundles.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+using System.IO;
+
+namespace Mix
+{
+	public class CreateAssetBundles
+	{
+		[MenuItem("Mix/Build AssetBundles")]
+		static void BuildAllAssetBundles()
+		{
+			string assetBundleDirectory = "Assets/AssetBundles";
+			if (!Directory.Exists(assetBundleDirectory))
+				Directory.CreateDirectory(assetBundleDirectory);
+
+			BuildPipeline.BuildAssetBundles(assetBundleDirectory,
+											BuildAssetBundleOptions.None,
+											BuildTarget.StandaloneWindows);
+		}
+	}
+}
